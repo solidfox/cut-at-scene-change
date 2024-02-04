f76db6902a8267140e1d7442df29367ddc1a0d4d
Daniel Schlaug <daniel@schlaug.com>
2024-02-04 21:10:47 +0100

    Rename and add caption handling

--- cut-at-timestamps.sh
+++ cut-at-timestamps.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 INPUT_VIDEO="$1"  # Use the first command line argument as the input video
-TIMESTAMPS_FILE="/tmp/scene_timestamps.txt"  # Assuming the timestamps file is in /tmp
+TIMESTAMPS_FILE="$2"  # Assuming the timestamps file is in /tmp
 COUNTER=1
 
 # Check if input video is provided
 if [ -z "$INPUT_VIDEO" ]; then
     echo "No input video provided."
+    exit 1
+fi
+
+# Check if timestamps file is provided
+if [ -z "$TIMESTAMPS_FILE" ]; then
+    echo "No timestamps file provided."
     exit 1
 fi
 
@@ -27,6 +33,7 @@ for line in "${TIMESTAMPS[@]}"
 do
     START_TIME=$(echo "$line" | cut -d' ' -f1)
     END_TIME=$(echo "$line" | cut -d' ' -f2)
+    CAPTION=$(echo "$line" | cut -d' ' -f3-)
     OUTPUT_VIDEO="${FILENAME}_cut_${COUNTER}.${EXTENSION}"
 
     echo
@@ -35,6 +42,7 @@ do
     echo "Cutting scene $COUNTER from $START_TIME to $END_TIME"
     echo
 
-    ffmpeg -i "$INPUT_VIDEO" -ss "$START_TIME" -to "$END_TIME" -c copy "$OUTPUT_VIDEO"
+    # Losslessly cut the video and add caption as metadata caption
+    ffmpeg -i "$INPUT_VIDEO" -ss "$START_TIME" -to "$END_TIME" -c copy -metadata caption="$CAPTION" "$OUTPUT_VIDEO"
     ((COUNTER++))
 done
