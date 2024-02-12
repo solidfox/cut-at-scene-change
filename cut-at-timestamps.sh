6697ae26a38056b83a8381d5e3b19d2731f5c600
Daniel Schlaug <daniel@schlaug.com>
2024-02-13 00:00:53 +0100

    Add output path argument and metadata

--- cut-at-timestamps.sh
+++ cut-at-timestamps.sh
@@ -2,6 +2,7 @@
 
 INPUT_VIDEO="$1"  # Use the first command line argument as the input video
 TIMESTAMPS_FILE="$2"  # Assuming the timestamps file is in /tmp
+OUTPUT_PATH="$3"  # Use the third command line argument as the output path
 COUNTER=1
 
 # Check if input video is provided
@@ -21,6 +22,11 @@ BASENAME=$(basename -- "$INPUT_VIDEO")
 FILENAME="${BASENAME%.*}"
 EXTENSION="${BASENAME##*.}"
 
+# Set the output path to the input video directory if not provided
+if [ -z "$OUTPUT_PATH" ]; then
+    OUTPUT_PATH=$(dirname "$INPUT_VIDEO")
+fi
+
 # Read timestamps into an array
 TIMESTAMPS=()
 while IFS= read -r line
@@ -34,7 +40,7 @@ do
     START_TIME=$(echo "$line" | cut -d' ' -f1)
     END_TIME=$(echo "$line" | cut -d' ' -f2)
     CAPTION=$(echo "$line" | cut -d' ' -f3-)
-    OUTPUT_VIDEO="${FILENAME}_cut_${COUNTER}.${EXTENSION}"
+    OUTPUT_VIDEO="${OUTPUT_PATH}/${FILENAME}_scene_${COUNTER}.${EXTENSION}"
 
     echo
     echo "----------------------------------------"
@@ -43,6 +49,25 @@ do
     echo
 
     # Losslessly cut the video and add caption as metadata caption
-    ffmpeg -i "$INPUT_VIDEO" -ss "$START_TIME" -to "$END_TIME" -c copy -metadata caption="$CAPTION" "$OUTPUT_VIDEO"
+    # Apple compatible metadata
+    # com.apple.quicktime.keywords: 
+    # com.apple.quicktime.description: test
+    # com.apple.quicktime.location.ISO6709: +59.2731+017.8017+000.000/
+    # com.apple.quicktime.creationdate: 2022-02-09T22:11:04+01:00
+    # https://developer.apple.com/documentation/quicktime-file-format/quicktime_metadata_keys
+    ffmpeg -i "$INPUT_VIDEO" \
+           -ss "$START_TIME" \
+           -to "$END_TIME" \
+           -c copy \
+           -metadata com.apple.quicktime.title="$FILENAME" \
+           -metadata com.apple.quicktime.keywords="camcorder, DV8" \
+           -metadata com.apple.quicktime.description="$CAPTION" \
+           -metadata com.apple.quicktime.information="Original Tape Name $FILENAME cut $COUNTER @ $START_TIME - $END_TIME"  \
+           "$OUTPUT_VIDEO"
+    # Error out of for look if ffmpeg fails
+    if [ $? -ne 0 ]; then
+        echo "Error cutting video at timestamp $line"
+        exit 1
+    fi
     ((COUNTER++))
 done
